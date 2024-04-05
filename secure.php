551694567cc8b3b5b19f4310767e0d46ed898ec5
wendylaiseowli <163742713+wendylaiseowli@users.noreply.github.com>
2024-04-06 00:09:19 +0800

    Session

--- secure.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-
-	//If the user is not signed in, they will be directed to index.php
-	if (!isset($_SESSION['username'])) {
-		header('Location: index.php');
-		exit();
-	}
-?>
---
+++ secure_Con.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+
+    // If the user is not signed in and their employee type is not "contractor", redirect them to index.php
+    if (!isset($_SESSION['username']) && $_SESSION['emp_type'] != "contractor") {
+        echo "<script>alert('You are not authorized to access this page.'); window.location='index.php';</script>";
+        exit();
+    }
+?>
