452b3648e03921763c5b640b21f2d42c8abf513c
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-04-13 01:18:19 +0800

    header

--- header.php
+++ header.php
@@ -1,32 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <div class="header-container">
-      <a class="navbar-brand" href="https://www.airselangor.com/" title="Air Selangor"><img src="as-logo.png"></a>
+      <a class="navbar-brand" href="https://www.airselangor.com/" title="Air Selangor"><img src="imgs/as-logo.png"></a>
       <h1>OTTO Aqua: Water Meter Inventory Management System</h1>
     </div>
-
-<div class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <div class="col-md-3 mb-2 mb-md-0">
-        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-          <svg class="bi" width="100%" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-        </a>
-      </div>
-
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2">Meter Track</a></li>
-        <li><a href="#" class="nav-link px-2">Meter Forecast</a></li>
-        <li><a href="#" class="nav-link px-2">Meter Test</a></li>
-      </ul>
-      
-    </header>
-  </div>
-
-<div>
-<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="#" title='Home'>Home</a></li>
-    <li class="breadcrumb-item active" aria-current="page"></li>
-  </ol>
-</nav>
-</div>
