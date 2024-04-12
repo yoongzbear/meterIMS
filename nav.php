9c898fa235db3e27f40cce7f3e47aed8b44c101a
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-04-13 02:47:38 +0800

    nav

--- nav.php
+++ nav.php
@@ -3,9 +3,9 @@
 
       <ul class="nav col-12 mb-2 justify-content-center mb-md-0">
         <li><a href="index.php" class="nav-link">Home</a></li>
-        <li><a href="track.php" class="nav-link">Meter Track</a></li>
-        <li><a href="forecast.php" class="nav-link">Meter Forecast</a></li>
-        <li><a href="test.php" class="nav-link">Meter Test</a></li>
+        <li><a href="mov_track_view.php" class="nav-link">Meter Track</a></li>
+        <li><a href="Inv_QRmenu.php" class="nav-link">Meter Forecast</a></li>
+        <li><a href="TestLab_QRmenu.php" class="nav-link">Meter Test</a></li>
       </ul>
 
     </header>
