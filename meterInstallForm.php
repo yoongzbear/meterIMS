eb48796d9f671541c05079768acc035ec98eea98
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-04-13 16:45:02 +0800

    Contractor Side

--- meterInstallForm.php
+++ meterInstallForm.php
@@ -1,11 +1,36 @@
+<?php 
+include 'secure_Con.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meter Installation Form</title>
+    <link href="styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
+
+<header>
+<?php 
+include 'header.php';
+include 'navCon.php';
+?>
+
+</header>
+
+<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="con_home.php" title='Home Page - Contractor'>Home</a></li>
+    <li class="breadcrumb-item"><a href="con_QRmenu.php" title='Meter Installation Page'>Meter Installation</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Meter Install Form</li>
+
+  </ol>
+</nav>
+
     <?php
         include('connection.php');
         $serial_num = $_POST['serial_num'];
@@ -35,5 +60,11 @@
             ";
         }
     ?>
+
 </body>
+
+<footer>
+	<?php include 'footer.php';?>
+</footer>	
+
 </html>
