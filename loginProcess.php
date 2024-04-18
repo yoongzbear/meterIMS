364a8309810cd5f72ddcbbde83c1f9b9a455a805
wendylaiseowli <163742713+wendylaiseowli@users.noreply.github.com>
2024-04-18 21:06:26 +0800

    Update loginProcess.php

--- loginProcess.php
+++ loginProcess.php
@@ -5,8 +5,21 @@
     if (isset($_POST['username'])) {
       $username = $_POST['username'];
       $password = $_POST['password'];
-      $sql = "SELECT * FROM useraccount WHERE username = '$username' AND password = '$password'";
+      $sql = "SELECT * 
+      FROM useraccount WHERE
+     useraccount.username = '$username' 
+      AND useraccount.password = '$password'";
+
       $result = mysqli_query($connection, $sql);
+
+      //for region store's location_id
+      $sql2 = "SELECT location.location_id 
+        FROM location, useraccount 
+        WHERE useraccount.username = location.username 
+        AND useraccount.username = '$username' AND useraccount.emp_type ='region store' ";
+      $result2 = mysqli_query($connection, $sql2);
+      $row2 = mysqli_fetch_assoc($result2);
+      $_SESSION['locationID'] =$row2['location_id'];
 
       // Check if there is a matching user
       if (mysqli_num_rows($result) == 1) {
@@ -15,6 +28,7 @@
       $_SESSION['username'] = $username;
       $_SESSION['emp_type'] = $row['emp_type'];
       $_SESSION['name'] = $row['name'];
+
   
         // Determine the home page based on emp_type
         if ($_SESSION['emp_type'] == 'region store') {
