f9f452c1dbe1da98c14f5dc872f2e61c1c97da29
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-04-22 22:33:47 +0800

    css home page

--- lab_home.php
+++ lab_home.php
@@ -1,7 +1,8 @@
 <?php 
 include 'secure_TestLab.php';
+include 'connection.php';
+$name = $_SESSION['name'];
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +12,8 @@ include 'secure_TestLab.php';
     <title>OTTO Aqua</title>
     <link href="styles.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
 </head>
 
 <body>
@@ -29,10 +32,97 @@ include 'navLab.php';
   </ol>
 </nav>
 
-</body>
+<div class="container text-center mb-4">
+  <h2 class="display-2">Welcome <?php echo $name ;?>!</h2>
+</div>
+
+<div class="container-fluid mb-2">
+  <div class="d-flex align-items-center ">
+    <div class='d-flex align-items-center'>
+      <i class="bi bi-chevron-double-left" style="font-size: 40px;margin-left:20px;"></i>
+    </div>
+    <div class='d-flex align-items-center'>  
+      <h3 style="font-size: 38px;">Air Selangor Inventory Department<large class="text-warning"> Features</large></h3>
+    </div>
+    <div class='d-flex align-items-center'>  
+      <i class="bi bi-chevron-double-right" style="font-size: 40px;"></i>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid">
+<!-- START THE FEATURETTES -->
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading fw-normal lh-1">Register new meters received from manufacturers.</h2>
+      <p class="lead">Generate new meter QR codes and assign meters into a new batch.</p>
+      <p>Quick access to register new meters <a href="inventoryDep_AddBatchForm.php">here</a>.</p>
+    </div>
+      <div class="col-md-5">
+        <img src="imgs/create-a-qr-code.png" width="700px" height="500px">
+      </div>
+  </div>
+
+<hr class="featurette-divider">
+
+  <div class="row featurette">
+    <div class="col-md-7 order-md-2">
+      <h2 class="featurette-heading fw-normal lh-1">Ship out meter batches.</h2>
+        <p class="lead">Scan the batch's QR code to record the batch movement to Test Lab or Region Stores.</p>
+        <p>Quick access to ship out meters to Test Lab <a href="">here</a>.</p>
+        <p>Quick access to ship out meters to Region Store <a href="">here</a>.</p>
+    </div>
+    <div class="col-md-5 order-md-1">
+      <img src="imgs/scan-delivery-box.jpg" width="700px" height="500px">
+    </div>
+  </div>
+
+<hr class="featurette-divider">
+
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading fw-normal lh-1">View water meter information.</h2>
+        <p class="lead">Scan meter's QR code to view their information or view their past lab tests' results.</p>
+        <p>Quick access to view meter's information <a href="InvDep_Scan_View_meter_info.php">here</a>.</p>
+        <p>Quick access to view meter's lab test result <a href="invMeterResult.php">here</a>.</p>
+    </div>
+    <div class="col-md-5">
+      <img src="imgs/scan-qr.jpg" width="500px" height="500px">
+    </div>
+  </div>
+
+<hr class="featurette-divider">
+
+  <div class="row featurette">
+    <div class="col-md-7 order-md-2">
+      <h2 class="featurette-heading fw-normal lh-1">Make forecast on water meter demand.</h2>
+        <p class="lead">Enter the water meter demand history and let the system generate a forecast for future meter demand.</p>
+        <p>Quick access to meter forecast <a href="">here</a>.</p>
+    </div>
+    <div class="col-md-5 order-md-1">
+      <img src="imgs/demand-forecast.png" width="700px" height="500px">
+    </div>
+  </div>
+
+<hr class="featurette-divider">
+
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading fw-normal lh-1">View meter's current movement.</h2>
+        <p class="lead">Track the movement of all meters in their batches. The details of the batch can also be viewed along with the list of meters in the batch.</p>
+        <p>Quick access to view meter movement tracking <a href="mov_track_view.php">here</a>.</p>
+    </div>
+    <div class="col-md-5">
+      <img src="imgs/movement-track.png" width="700px" height="500px">
+    </div>
+  </div>
+<!-- /END THE FEATURETTES -->
+</div>
 
 <footer>
 	<?php include 'footer.php';?>
 </footer>	
+
+</body>
 
 </html>
