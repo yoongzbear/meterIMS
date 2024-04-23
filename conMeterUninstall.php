761bc1db93aac6fdcf592fd8b6911be035f9c29c
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-04-23 17:41:34 +0800

    css

--- conMeterUninstall.php
+++ conMeterUninstall.php
@@ -77,11 +77,11 @@ if(ISSET($_POST['serialnum'])){
         </div>
 
         <div id="meterForm" class="col-lg-12 mt-5 border border-warning shadow mb-4 rounded" style="display: none; width: 50%; margin:auto;">
-            <h3 class="text-center">Please confirm that the Serial Number is correct.</h3>
+            <h3 class="text-center">Please Confirm that the Serial Number is Correct.</h3>
             <form method="POST" name="serialNumForm" class="text-center">
                 <label>Meter ID : </label>
                 <input type="text" id="outputData" name="serialnum" placeholder="Serial Number" required readonly><br>
-                <button type="submit" class="btn btn-success m-2 pt-1 pb-1 mt-4">Mark as uninstalled</button>
+                <button type="submit" class="btn btn-success m-2 pt-1 pb-1 mt-4">Mark As Uninstalled</button>
             </form>
         </div>
         
