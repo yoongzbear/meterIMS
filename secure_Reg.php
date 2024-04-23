11d1d27961ca0d3799187280ace72c11c7fc396a
wendylaiseowli <163742713+wendylaiseowli@users.noreply.github.com>
2024-04-23 21:28:23 +0800

    Update secure_Reg.php

--- secure_Reg.php
+++ secure_Reg.php
@@ -2,7 +2,7 @@
     session_start();
 
     // If the user is not signed in and their employee type is not "region store", redirect them to index.php
-    if (!isset($_SESSION['username']) && $_SESSION['emp_type'] != "region store") {
+    if (!isset($_SESSION['username']) || $_SESSION['emp_type'] != "region store") {
         echo "<script>alert('You are not authorized to access this page.'); window.location='index.php';</script>";
         exit();
     }
