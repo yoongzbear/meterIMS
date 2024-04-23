76efa9069c11c584f9d084a2d33985b1b112035a
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-04-24 04:25:08 +0800

    breadcrumb

--- meterInstall.php
+++ meterInstall.php
@@ -24,7 +24,7 @@ include 'navCon.php';
 <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="con_home.php" title='Home Page - Contractor'>Home</a></li>
-    <li class="breadcrumb-item"><a href="con_QRmenu.php" title='Meter Installation Page'>Meter Installation</a></li>
+    <li class="breadcrumb-item"><a href="con_QRmenu.php" title='QR Page'>QR Code</a></li>
     <li class="breadcrumb-item active" aria-current="page">Scan QR - Meter Installation</li>
 
   </ol>
