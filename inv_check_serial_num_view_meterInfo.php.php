f790b466bc7d35eaa1512687445247ff18bccad7
wendylaiseowli <163742713+wendylaiseowli@users.noreply.github.com>
2024-04-23 21:00:53 +0800

    Add files via upload

---
+++ inv_check_serial_num_view_meterInfo.php.php
@@ -0,0 +1,22 @@
+<?php
+include 'secure_Inv.php';
+include 'connection.php';
+
+if(isset($_POST['serial_num'])) {
+    $serial_num = $_POST['serial_num'];
+
+    // Query to check if the serial num exists in the database
+    $query = "SELECT * FROM meter WHERE serial_num = '$serial_num'";
+    $result = mysqli_query($connection, $query);
+
+    // Check if the query returned any rows
+    if(mysqli_num_rows($result) > 0) {
+        // serial num exists
+        header("Location:InvDep_view_meter_info.php?serial_num= $serial_num");
+        exit();
+    } else {
+        // serial num does not exist
+        echo '<script>alert("Serial number not exist. Please try again."); window.location.href = "InvDep_Scan_View_meter_info.php";</script>';
+    }
+}
+?>
