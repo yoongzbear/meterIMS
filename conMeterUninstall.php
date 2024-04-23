7832185e543f04616656c6cfa1da94b37e5442fc
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-04-23 16:50:21 +0800

    css

--- conMeterUninstall.php
+++ conMeterUninstall.php
@@ -62,9 +62,9 @@ if(ISSET($_POST['serialnum'])){
     </header>
     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
     <ol class="breadcrumb">
-        <li class="breadcrumb-item active" aria-current="page">Home</li>
-        <li class="breadcrumb-item active" aria-current="page">Meter Installation</li>
-        <li class="breadcrumb-item active" aria-current="page">Meter Uninstallation</li>
+    <li class="breadcrumb-item"><a href="con_home.php" title='Home Page - Contractor'>Home</a></li>
+        <li class="breadcrumb-item"><a href="con_QRmenu.php" title='Meter Installation Page'>Meter Installation</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Scan QR - Meter Uninstallation</li>
     </ol>
     </nav>
     <section id="meteruninstall">
