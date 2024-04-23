1c965ad1f4aaee4a4460dbd9858b64313fce9402
wendylaiseowli <163742713+wendylaiseowli@users.noreply.github.com>
2024-04-23 21:28:37 +0800

    Update secure_TestLab.php

--- secure_TestLab.php
+++ secure_TestLab.php
@@ -2,7 +2,7 @@
     session_start();
 
     // If the user is not signed in and their employee type is not "lab tester", redirect them to index.php
-    if (!isset($_SESSION['username']) && $_SESSION['emp_type'] != "lab tester") {
+    if (!isset($_SESSION['username']) || $_SESSION['emp_type'] != "lab tester") {
         echo "<script>alert('You are not authorized to access this page.'); window.location='index.php';</script>";
         exit();
     }
