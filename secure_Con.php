7e78effbcebd8649cb6302f9fe0ba6bfe4ee7718
wendylaiseowli <163742713+wendylaiseowli@users.noreply.github.com>
2024-04-23 21:27:50 +0800

    Update secure_Con.php

--- secure_Con.php
+++ secure_Con.php
@@ -2,7 +2,7 @@
     session_start();
 
     // If the user is not signed in and their employee type is not "contractor", redirect them to index.php
-    if (!isset($_SESSION['username']) && $_SESSION['emp_type'] != "contractor") {
+    if (!isset($_SESSION['username']) || $_SESSION['emp_type'] != "contractor") {
         echo "<script>alert('You are not authorized to access this page.'); window.location='index.php';</script>";
         exit();
     }
