47940e80f01528d2ee2dae43446be239ad73a3f9
wan064 <155166351+wan064@users.noreply.github.com>
2024-04-24 17:37:35 +0800

    Add files via upload

---
+++ inv_ShipMeterCheckQR.php
@@ -0,0 +1,24 @@
+<?php
+include 'secure_Reg.php';
+include 'connection.php';
+
+if(isset($_GET['Meter_ID'])) {
+    $batch_id = $_GET['batch_id'];
+    $serial_num = $_GET['Meter_ID'];
+    $meterQuantity = $_GET['meterQuantity'];
+
+    // Query to check if the serial num exists in the database
+    $query = "SELECT * FROM meter WHERE serial_num = '$serial_num'";
+    $result = mysqli_query($connection, $query);
+
+    // Check if the query returned any rows
+    if(mysqli_num_rows($result) > 0) {
+        // serial num exists
+        header("Location:invDep_ShipAddMeter.php?Meter_ID=$serial_num&Batch_ID=$batch_id&meterQuantity=$meterQuantity");
+        exit();
+    } else {
+        // serial num does not exist
+        echo '<script>alert("Serial number does not exist. Please try again."); window.location.href = "inventoryDep_ShipOrderScanMeterQR.php?Batch_ID='.$batch_id.'&meterQuantity='.$meterQuantity.'";</script>';
+    }
+}
+?>
