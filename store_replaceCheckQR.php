28399cba6d4018fe44f026eacba20a1ccfbbfdd4
wan064 <155166351+wan064@users.noreply.github.com>
2024-04-24 23:24:13 +0800

    Add files via upload

---
+++ store_replaceCheckQR.php
@@ -0,0 +1,47 @@
+<?php
+include 'secure_Reg.php';
+include 'connection.php';
+
+if(isset($_GET['Meter_ID'])) {
+    $serial_num = $_GET['Meter_ID'];
+	$oldSerial_num = $_GET['oldSerial_num'];
+
+    //check for new meter
+    $sqlNew = "SELECT batch.* FROM batch 
+			INNER JOIN meter ON meter.batch_id = batch.batch_id
+			WHERE meter.serial_num = '$serial_num'";
+    $resultNew = mysqli_query($connection, $sqlNew);
+	
+	//check for old meter
+    $sqlOld = "SELECT batch.* FROM batch 
+			INNER JOIN meter ON meter.batch_id = batch.batch_id
+			WHERE meter.serial_num = '$oldSerial_num'";
+    $resultOld = mysqli_query($connection, $sqlOld);
+
+    // Check if the query returned any rows
+    if(mysqli_num_rows($resultNew) > 0 && mysqli_num_rows($resultOld) > 0) {
+        // serial num exists
+		$rowNew = mysqli_fetch_assoc($result);
+		$newMeter_type = $rowNew['meter_type'];
+		$newMeter_model = $rowNew['meter_model'];
+		$newMeter_size = $rowNew['meter_size'];
+		
+		$rowOld = mysqli_fetch_assoc($result);
+		$oldMeter_type = $rowOld['meter_type'];
+		$oldMeter_model = $rowOld['meter_model'];
+		$oldMeter_size = $rowOld['meter_size'];
+		
+		if ($newMeter_type == $oldMeter_type && $newMeter_model == $oldMeter_model && $newMeter_size == $oldMeter_size) {
+			header("Location:store_replaceMeterComplete.php?Meter_ID=$serial_num&oldSerial_num=$oldSerial_num");
+			exit();
+		} else {
+			echo '<script>alert("The meter type, model, or size do not match the old meter."); window.location.href = "store_replaceMeterScanQR.php?serial_num='.$oldSerial_num.'";</script>';
+		}
+		
+        
+    } else {
+        // serial num does not exist
+        echo '<script>alert("Serial number does not exist. Please try again."); window.location.href = "store_replaceMeterScanQR.php?serial_num='.$oldSerial_num.'";</script>';
+    }
+}
+?>
---
+++ store_replaceMeterComplete.php
@@ -0,0 +1,134 @@
+<?php
+	//include('secure_Inv.php');
+	include('connection.php');
+	$serial_num = $_GET['Meter_ID'];
+	$oldSerial_num = $_GET['oldSerial_num'];
+	
+	//Update Old Meter Warranty Status
+	$sqlUpdate = "UPDATE warranty SET warranty_status = 'REPLACED' WHERE serial_num = '$oldSerial_num'";
+	$resultUpdate = mysqli_query($connection,$sqlUpdate);
+	
+	if($resultUpdate){
+		echo "<script>alert('Meter status updated successfully!');</script>";
+	}
+	
+	//Update New Meter Status
+	$sqlUpdate = "UPDATE meter SET meter_status = 'TO BE INSTALLED' WHERE serial_num = '$serial_num'";
+	$resultUpdate = mysqli_query($connection,$sqlUpdate);
+	
+	if($resultUpdate){
+		echo "<script>alert('Meter assigned successfully!');</script>";
+	}
+	
+	//To get New Meter Info
+	$sql = "SELECT batch.*, meter.*, manufacturer.* FROM meter 
+					INNER JOIN batch ON meter.batch_id = batch.batch_id 
+					INNER JOIN manufacturer ON meter.manu_id = manufacturer.manu_id
+					WHERE serial_num = '$serial_num'";
+	$result = mysqli_query($connection, $sql);
+	
+	if ($result) {
+		$row = mysqli_fetch_assoc($result);
+
+		// Fetch data from the database
+		$meter_type = $row["meter_type"];
+		$meter_model = $row["meter_model"];
+		$meter_size = $row["meter_size"];
+		$age = $row["age"];
+		$mileage = $row["mileage"];
+		$manu_name = $row["manu_name"];
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OTTO Aqua</title>
+    <link href="styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<header>
+<?php 
+include 'header.php';
+?>
+
+</header>
+
+
+<div class="container">
+    <div class="row align-items-start">
+        <div class="col">
+
+	<!--Meter Info-->
+	<h3>Meter Information</h3>
+	<hr>
+	<table class="table table-secondary">
+		<tr class="table" colspan = "2">
+			<td>
+				<div id="qrcode">
+					<script src = "qrcode.js"></script>
+					<script src = "qrGeneratorMeter.js"></script>
+					<script>makeCode(); </script>
+				</div>
+			</td>
+		</tr>
+
+		<thread>
+		<tr>
+			<th scope="row">Meter Type</th>
+			<td><?php echo $meter_type; ?></td>
+		</tr>
+		</thread>
+		<thread>
+		<tr>
+			<th scope="row">Meter Model</th>
+			<td><?php echo $meter_model; ?></td>
+		</tr>
+		</thread>
+		<thread>
+		<tr>
+			<th scope="row">Meter Size</th>
+			<td><?php echo $meter_size; ?></td>
+		</tr>
+		</thread>
+		<thread>
+		<tr>
+			<th scope="row">Age</th>
+			<td><?php echo $age; ?></td>
+		</tr>
+		</thread>
+		<thread>
+		<tr>
+			<th scope="row">Mileage</th>
+			<td><?php echo $mileage; ?></td>
+		</tr>
+		</thread>
+		<thread>
+		<tr>
+			<th scope="row">Manufacturer</th>
+			<td><?php echo $manu_name; ?></td>
+		</tr>
+		</thread>
+	</table>
+	</div>
+
+	</div>
+</div>
+
+<div class="d-grid gap-2 col-6 mx-auto">
+	<button class="back btn btn-dark" type="button" onclick="window.location.href='inventoryDep_AddBatchForm.php'" title='Back To Create New Batch Page'>Back</button>
+</div>
+	
+<br>
+
+</body>
+
+<footer>
+	<?php include 'footer.php';?>
+</footer>	
+
+</html>
