ac48d291b98ffc182694c708dcdc2dcde2e823cb
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-04-25 08:38:32 +0800

    breadcrumb

--- regInvLevel.php
+++ regInvLevel.php
@@ -25,14 +25,14 @@
     </header>
     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
     <ol class="breadcrumb">
-        <li class="breadcrumb-item active" aria-current="page">Home</li>
+        <li class="breadcrumb-item"><a href="reg_home.php" title='Home Page - Region Store'>Home</a></li>
         <li class="breadcrumb-item active" aria-current="page">View Inventory Level</li>
     </ol>
     </nav>
-    <section id="InvTable">
+    <section id="InvTable" class="mb-4">
         <div id="container" style="width:50%; margin:auto;">
         <div class="col-lg-12 mt-4 text-center">
-            <h2>Regional Store Inventory Level</h2>
+            <h2 class="display-6">Regional Store Inventory Level</h2>
             <h3 class="mt-4">Location: <?php echo $location; ?></h3>
         </div>
         <div class="col-lg-12 mt-4 text-center">
