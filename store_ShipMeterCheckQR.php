afb28ac21b879bab2344d9b4cee7d0ead5880c4c
wan064 <155166351+wan064@users.noreply.github.com>
2024-04-26 08:24:38 +0800

    check if meter scanned ady exists

--- store_ShipMeterCheckQR.php
+++ store_ShipMeterCheckQR.php
@@ -13,9 +13,15 @@ if(isset($_GET['Meter_ID'])) {
 
     // Check if the query returned any rows
     if(mysqli_num_rows($result) > 0) {
-        // serial num exists
-        header("Location:store_ShipAddMeter.php?Meter_ID=$serial_num&Batch_ID=$batch_id&meterQuantity=$meterQuantity");
-        exit();
+        //To check if the meter scanned exist in the batch
+        $sqlMeterExist = "SELECT * FROM meter WHERE serial_num = '$serial_num' AND batch_id = '$batch_id'";
+        $resultMeterExist = mysqli_query($connection, $sqlMeterExist);
+        if(mysqli_num_rows($resultMeterExist) > 0) {            // serial num exists
+            header("Location:store_ShipAddMeter.php?Meter_ID=$serial_num&Batch_ID=$batch_id&meterQuantity=$meterQuantity");
+            exit();
+        } else {
+            echo '<script>alert("Meter have been scanned. Please try again wuth other meter."); window.location.href = "store_ShipOrderScanMeterQR.php?Batch_ID='.$batch_id.'&meterQuantity='.$meterQuantity.'";</script>';
+        }
     } else {
         // serial num does not exist
         echo '<script>alert("Serial number does not exist. Please try again."); window.location.href = "store_ShipOrderScanMeterQR.php?Batch_ID='.$batch_id.'&meterQuantity='.$meterQuantity.'";</script>';
