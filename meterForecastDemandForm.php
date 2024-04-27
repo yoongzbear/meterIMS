7705789df67b16d9e89f37f30220c6569226500a
wan064 <155166351+wan064@users.noreply.github.com>
2024-04-27 22:41:07 +0800

    validation

--- meterForecastDemandForm.php
+++ meterForecastDemandForm.php
@@ -16,16 +16,10 @@
 			<tr>
 				<td>Forecast Year</td>
 				<td>
-					<select class="form-control" name="year" id="year" required>
-						<option value="" disabled selected>Please Select Meter Forecast Year</option>
-							<?php
-							$currentYear = date('Y');
-
-							//Loop to generate options for next 2 years
-							for($i = $currentYear + 1; $i <= $currentYear + 2; $i++) {
-								echo "<option value=\"$i\">$i</option>";
-							}
-							?>
+					<select name="year" id="year" required>
+						<option value="" disabled selected>Select Year for Meter Forecast</option>
+						<option value="2025">2025</option>
+						<option value="2026">2026</option>
 					</select>
 				</td>
 			</tr>
@@ -117,7 +111,6 @@
 			var newFilename = 'meterdemand';
             file.name = newFilename;
 			// Rename the file before submission
-			alert("File Uploaded Successfully!");
         });
     });
 </script>
