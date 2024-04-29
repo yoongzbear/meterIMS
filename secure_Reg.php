9f3afa2b3570ae32f1045621fd1f4b63e1111fe5
wan064 <155166351+wan064@users.noreply.github.com>
2024-04-29 10:04:38 +0800

    comments

--- secure_Reg.php
+++ secure_Reg.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    // If the user is not signed in and their employee type is not "region store", redirect them to index.php
+    //If the user is not signed in and their employee type is not "region store", redirect them to index.php
     if (!isset($_SESSION['username']) || $_SESSION['emp_type'] != "region store") {
         echo "<script>alert('You are not authorized to access this page.'); window.location='index.php';</script>";
         exit();
