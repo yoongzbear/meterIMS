23c3ea419398558048636555a02b2d56fe1ecd53
wan064 <155166351+wan064@users.noreply.github.com>
2024-05-20 22:48:25 +0800

    new sql

--- store_ShipAddBatch.php
+++ store_ShipAddBatch.php
@@ -7,21 +7,21 @@
 	$meter_size = $meter_details[2];
 	
 	$meterQuantity = $_POST["meterQuantity"];
-	$origin = $_POST["origin"];
-	$destination = $_POST["destination"];
+	$outbound_id = $_POST["outbound_id"];
+	$inbound_id = $_POST["inbound_id"];
 	$ship_date = $_POST["ship_date"];
 	
 	//Insert meter batch info
 	$sqlBatch = "INSERT INTO batch (location_id, meter_type, meter_model, meter_size) 
-			VALUES ('$origin', '$meter_type', '$meter_model', '$meter_size')";
+			VALUES ('$outbound_id', '$meter_type', '$meter_model', '$meter_size')";
 	$result = mysqli_query($connection, $sqlBatch);
 	
 	if ($result) {
 		$batch_id = mysqli_insert_id($connection);
 		
 		//Insert tracking details
-		$sqlTracking = "INSERT INTO movement (origin, destination, ship_date, batch_id) VALUES
-				('$origin', '$destination', '$ship_date', '$batch_id')";
+		$sqlTracking = "INSERT INTO movement (outbound, inbound, ship_date, batch_id) VALUES
+				('$outbound_id', '$inbound_id', '$ship_date', '$batch_id')";
 		$resultTrack = mysqli_query($connection, $sqlTracking);
 		echo "<script>window.location.href = 'store_ShipOrderScanMeterQR.php?Batch_ID=$batch_id&meterQuantity=$meterQuantity';</script>"; 
 	} else {
