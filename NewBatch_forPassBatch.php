d15492fb611e886b2d08d9698a67197e090975a6
wendylaiseowli <163742713+wendylaiseowli@users.noreply.github.com>
2024-05-27 00:51:23 +0800

    Add files via upload

---
+++ NewBatch_forPassBatch.php
@@ -0,0 +1,47 @@
+<?php
+include 'secure_TestLab.php';
+include 'connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Warranty Batch ID Printing</title>
+    <link href="styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <?php include 'header.php'; ?>
+</header>
+
+<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+<ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="lab_home.php" title='Home Page - Test Lab'>Home</a></li>
+        <li class="breadcrumb-item"><a href="TestLab_QRmenu.php" title='QR Code Menu'>QR Code</a></li>
+        <li class="breadcrumb-item"><a href="LabDep_Scan_to_Inv.php" title='Scan QR Page'>Scan QR - Send To Inventory Department</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Confirmation For Send To Inventory Department</li>
+    </ol>
+</nav>
+
+<div class="col align-self-center mb-4">
+	<h3>Batch QR:</h3>
+	<div id="qrcode">
+		<script src="qrcode.js"></script>
+		<script src="qrGeneratorBatch.js"></script>
+		<script>
+		    makeCode(); //calling function from js to generate QR code
+		    print(); //invoke browser print function
+		</script>
+	</div>
+</div>
+
+<footer>
+	<?php include 'footer.php';?>
+</footer>	
+
+</body>
+</html>
